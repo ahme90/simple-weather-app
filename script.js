4f7cdb1fa414fc91f6163456ca18927cb37ffb7d
ahmedgjg <ahme90569@gmail.com>
2024-09-12 22:48:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.getElementById("save").addEventListener("click", function(){
+//     var city= document.getElementById("data").value;
+//     fetch(" http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=0515fa3a97c1dd00f5313a2d038a0a24 ")
+//     .then(response=>response.json())
+//     .then(data=>{
+//         document.getElementById("a").innerHTML = data.sys.country;
+//         document.getElementById("b").innerHTML = (new Date((data.sys.sunrise)*1000)).toLocaleString();
+//         document.getElementById("c").innerHTML = (new Date((data.sys.sunset)*1000)).toLocaleString();
+//         document.getElementById("d").innerHTML=((data.main.temp) -273). toFixed(0)+"<sup>0</sup>C";
+//         })
+// })  
+// document-getElementById("save") -addEventListener("click", function(
+//     var city=document.getElementById("data") -value;
+//     fetch ("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid-0515fa3a97c1dd00f5313a2d038a0a24*)
+//     -then (response=>response.json())
+//     - then(data=>
+//     document-getElementById("a"). innerHTML-data.name+" "+data.sys.country;
+//     document-getElementById("b") -innerHTML=(new Date((data.sys.sunrise)*1000)).toLocaleString();
+//     document-getElementById("c").innerHTML=(new Date((data.sys.sunset)*1000)) -toLocaleString();
+//     document-getElementById (d).innerHTML=((data.main.temp) -273). toFixed(®)+*<sup>0</sup>C";
+//     document-getElementById("e").innerHTML=data.weather[0].id
+    
